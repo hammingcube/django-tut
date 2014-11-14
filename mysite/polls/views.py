2f8c1b8fe6c4bb0658f2e493773cb8229dd7464b
Madhav Jha <mjha@zenefits.com>
2014-11-13 18:28:58 -0800

    moving to templates based view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question
+from django.template import RequestContext, loader
 
 def index(request):
 	questions = Question.objects.all()
-	qlist = [q.question_text for q in questions]
-	response = ','.join(qlist)
-	return HttpResponse(response)
-
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+		'question_list' : questions,
+	})
+	return HttpResponse(template.render(context))
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
