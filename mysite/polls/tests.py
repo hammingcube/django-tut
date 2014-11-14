c5217e06f662984234fa337883c8c9a5db0446c2
Madhav Jha <mjha@zenefits.com>
2014-11-13 21:43:37 -0800

    tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from polls.models import Question
 
-# Create your tests here.
+class QuestionMethodTests(TestCase):
+	def test_question_with_empty_text(self):
+		"""
+		question_text should be non-empty for every
+		Question object
+		"""
+		q = Question()
+		self.assertEqual(len(q.question_text), 0)
+
